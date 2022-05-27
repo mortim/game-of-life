8a90072ba51bba1a4358c34984855609fe8aec40
mortim <aouladkarim010@gmail.com>
2022-05-27 23:10:47 +0200

    Recoding the main file in oop

--- main.py
+++ main.py
@@ -1,138 +1,146 @@
 import pygame
 import rle
 
-print("1) Load a RLE file\n2) Continue without load a file\n3) Quit the program\n")
-loop = True
-load_file_option = False
-filepath = ""
-while loop:
-    choice = input("> ")
-    if choice == "1":
-        filepath = input("filepath: ")
-        load_file_option = True
-        loop = False
-    elif choice == "2":
-        loop = False
-    elif choice == "3":
-        exit()
-    else:
-        print("The command is unknown")
+class GOL:
+    def __init__(self):
+        self.cell_size = 15
+        self.window_size = (80,60)
+        self.grid = {}
+        self.tab_birth = []
+        self.tab_death = []
+        self.filepath = ""
+        self.load_option = False
+        # ----
+        self.screen = None
+        pygame.display.set_caption("game of life")
+        self.clock = pygame.time.Clock()
+        
+    def generate(self):
+        self.screen.fill((255,255,255))
+        for x in range(int(self.window_size[0])):
+            for y in range(int(self.window_size[1])):
+                self.grid[(x,y)] = False
+                pygame.draw.rect(self.screen, (0,0,0), pygame.Rect(x*self.cell_size,y*self.cell_size,self.cell_size,self.cell_size),1)
+    
+    def set_state(self,coord,state):
+        surf = pygame.Surface((self.cell_size-2,self.cell_size-2))
+        if state:
+            surf.fill((0,0,0))
+            self.grid[coord] = True
+        else:
+            surf.fill((255,255,255))
+            self.grid[coord] = False
+        self.screen.blit(surf, (coord[0]*self.cell_size+1, coord[1]*self.cell_size+1))
 
-pygame.init()
+    def neighbours(self,coord):
+        x,y=coord
+        coords=[(x-1,y-1), (x,y-1), (x+1,y-1), (x-1,y), (x+1,y), (x-1,y+1), (x,y+1), (x+1,y+1)]
+        alive=0
+        for c in coords:
+            # Check if the neighbour cell is in the grid
+            if not((c[0] < 0 or c[1] < 0) or (c[0] > self.window_size[0]-1 or c[1] > self.window_size[1]-1)):
+                if self.grid[c]: alive += 1
+        return alive
 
-# Initial configuration
-CELL_SIZE=15
-WINDOW_SIZE=(80,60)
-TAB_BIRTH=[]
-TAB_DEATH=[]
-# ---
-screen = pygame.display.set_mode((WINDOW_SIZE[0]*CELL_SIZE, WINDOW_SIZE[1]*CELL_SIZE))
-pygame.display.set_caption("Conway_gol")
-clock = pygame.time.Clock()
+    def convert_mouse_coords_on_grid(self,mouse_coords):
+        x,y = mouse_coords
+        return (x//self.cell_size,y//self.cell_size)
+    
+    def menu(self):
+        print("1) Load a RLE file\n2) Continue without load a file\n3) Quit the program\n")
+        while True:
+            choice = input("> ")
+            if choice == "1":
+                self.filepath = input("filepath: ")
+                self.load_option = True
+                return
+            if choice == "2":
+                return
+            if choice == "3":
+                exit()
+            print("The command is unknown")
 
-# Generate the cell grid
-def generate(w,h,size):
-    screen.fill((255,255,255))
-    res={}
-    for x in range(w):
-        for y in range(h):
-            res[(x,y)] = False
-            pygame.draw.rect(screen, (0,0,0), pygame.Rect(x*size,y*size,size,size),1)
-    return res
+    def simulate(self):
+        loop=True
+        while loop:
+            for event in pygame.event.get():
+                # -------------------------------
+                if event.type == pygame.QUIT: loop = False
+                # -------------------------------
+                elif pygame.mouse.get_pressed()[0]:
+                    cell_coords = self.convert_mouse_coords_on_grid(pygame.mouse.get_pos())
+                    self.set_state(cell_coords, not self.grid[cell_coords])
+                # -------------------------------
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    loop_=True
+                    while loop_:
+                        if all(map(lambda x: not x, self.grid.values())):
+                            loop_ = False
 
-GRID = generate(*WINDOW_SIZE,CELL_SIZE)
+                        for event in pygame.event.get():
+                            # Exit the program during the simulation
+                            if event.type == pygame.QUIT:
+                                loop_ = False
+                                loop = False
+                            # Pause the simulation
+                            elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                                l=True
+                                while l:
+                                    for event in pygame.event.get():
+                                        # Exit the program when the simulation is paused
+                                        if event.type == pygame.QUIT:
+                                            l = False
+                                            loop_ = False
+                                            loop = False
 
-# Set a cell alive or dead 
-def setState(coord,state):
-    surf = pygame.Surface((CELL_SIZE-2,CELL_SIZE-2))
-    if state:
-        surf.fill((0,0,0))
-        GRID[coord] = True
-    else:
-        surf.fill((255,255,255))
-        GRID[coord] = False
-    screen.blit(surf, (coord[0]*CELL_SIZE+1, coord[1]*CELL_SIZE+1))
+                                        elif pygame.mouse.get_pressed()[0]:
+                                            cell_coords = self.convert_mouse_coords_on_grid(pygame.mouse.get_pos())
+                                            self.set_state(cell_coords, not self.grid[cell_coords])
+                                            pygame.display.flip()
+                                        # Resume the simulation
+                                        elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                                            l = False
+        
+                        for (coord,_) in self.grid.items():
+                            if self.grid[coord]:
+                                # A cell survive only if he has 2 or 3 alive neighbours cells
+                                if not(self.neighbours(coord) == 3 or self.neighbours(coord) == 2):
+                                    self.tab_death.append(coord)
+                            else:
+                                # If a dead cell has 3 alive neighbours cells, he wil become alive
+                                if self.neighbours(coord) == 3:
+                                    self.tab_birth.append(coord)
 
-# Get the number of the alive neighbours cells
-def neighbours(coord):
-    x,y=coord
-    coords=[(x-1,y-1), (x,y-1), (x+1,y-1), (x-1,y), (x+1,y), (x-1,y+1), (x,y+1), (x+1,y+1)]
-    alive=0
-    for c in coords:
-        # Check if the neighbour cell is in the grid
-        if not((c[0] < 0 or c[1] < 0) or (c[0] > WINDOW_SIZE[0]-1 or c[1] > WINDOW_SIZE[1]-1)):
-            if GRID[c]: alive += 1
-    return alive
+                        for n in self.tab_birth:
+                            self.set_state(n,True)
+                        self.tab_birth=[]
 
-def changeCellState(coords):
-    (x,y) = coords
-    cell_coords=(x//CELL_SIZE,y//CELL_SIZE)
-    if GRID[cell_coords]:
-        setState(cell_coords, False)
-    else:
-        setState(cell_coords, True)
+                        for m in self.tab_death:
+                            self.set_state(m,False)
+                        self.tab_death=[]
 
-if load_file_option:
-    r = rle.RLE(filepath,(10,10))
-    output = r.parse()
-    print(f"Loading the '{filepath}' file...")
-    print(f"""
-The pattern '{output["name"]}' is loaded by {output["author"]}
-{output["description"]}
-    """)
-    for (x,y) in output["cells"]:
-        setState((x,y), True)
-    pygame.display.flip()
+                        pygame.display.flip()
+                        pygame.time.delay(100)
+            pygame.display.flip()
+            self.clock.tick(60)
 
-loop=True
-while loop:
-    for event in pygame.event.get():
-        # -------------------------------
-        if event.type == pygame.QUIT: loop = False
-        # -------------------------------
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            changeCellState(pygame.mouse.get_pos())
-        # -------------------------------
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            while loop:
-                for event in pygame.event.get():
-                    # Exit the program during the simulation
-                    if event.type == pygame.QUIT: loop = False
-                    # Pause the simulation
-                    elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
-                        l=True
-                        while l:
-                            for event in pygame.event.get():
-                                # Exit the program when the similation is paused
-                                if event.type == pygame.QUIT:
-                                    l=False
-                                    loop = False
-                                elif event.type == pygame.MOUSEBUTTONDOWN:
-                                    changeCellState(pygame.mouse.get_pos())
-                                    pygame.display.flip()
-                                # Resume the simulation
-                                elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
-                                    l = False
+    def run(self):
+        self.menu()
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.window_size[0]*self.cell_size, self.window_size[1]*self.cell_size))
+        self.generate()
+        # ----
+        if self.load_option:
+            r = rle.RLE(self.filepath,self.window_size)
+            output = r.parse()
+            print(f"Loading the '{self.filepath}' file...")
+            print(f"\nThe pattern '{output['name']}' is loaded by {output['author']}\n{output['description']}")
+            # ---
+            for (x,y) in output["cells"]:
+                self.set_state((x,y), True)
+            pygame.display.flip()
+        # ----
+        self.simulate()
 
-                for (coord,_) in GRID.items():
-                    if GRID[coord]:
-                        # A cell survive only if he has 2 or 3 alive neighbours cells
-                        if not(neighbours(coord) == 3 or neighbours(coord) == 2):
-                            TAB_DEATH.append(coord)
-                    else:
-                        # If a dead cell has 3 alive neighbours cells, he wil become alive
-                        if neighbours(coord) == 3:
-                            TAB_BIRTH.append(coord)
-
-                for n in TAB_BIRTH:
-                    setState(n,True)
-                TAB_BIRTH=[]
-
-                for m in TAB_DEATH:
-                    setState(m,False)
-                TAB_DEATH=[]
-
-                pygame.display.flip()
-                pygame.time.delay(100)
-    pygame.display.flip()
-    clock.tick(60)
+game_of_life = GOL()
+game_of_life.run()
--- rle.py
+++ rle.py
@@ -3,17 +3,15 @@ import re
 class RLE:
     def __init__(self,filename,coords):
         self.filename = filename
-        (self.x,self.y) = coords
+        self.x,self.y = coords
         # ---
         self.patterns = [
             r"(x = [0-9]+), (y = [0-9]+)(, rule = [a-zA-Z/]+)*",
             r"([0-9]*)(b+|o+)"
         ]
-        self.output = {"name": "", "author": "", "description": "", "size":(),"cells":[]}
+        self.output = {"name": "", "author": "", "description": "","cells":[]}
 
     def parse(self):
-        w = self.x
-
         try:
             with open(self.filename, "r") as f:
                 for line in f.read().split("\n"):
@@ -22,7 +20,8 @@ class RLE:
                     if m:
                         (a,b,_) = m[0]
                         x,y = int(a.split()[-1]), int(b.split()[-1])
-                        self.output["size"] = (x,y)
+                        self.x = (self.x - x)//2
+                        self.y = (self.y - y)//2
 
                     elif line[0] != "#":
                         tmp = self.x
